761addf40dbb6786cac9d7416f63a3e1b7657b87
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-06-24 14:11:22 +0100

    made a change so that the fetch uses a variable instead of raw text and shortened the second then method

--- API-BoredBot/index.js
+++ API-BoredBot/index.js
@@ -1,11 +1,7 @@
+const urlDogAPI = 'https://dog.ceo/api/breeds/image/random'
 
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        document.querySelector('.image-div').innerHTML = `
-        <img src="${data.message}"/>
-        `
-    })
-
-
+fetch(urlDogAPI)
+.then(response => response.json())
+.then(data => document.querySelector('.image-div').innerHTML = `
+    <img src='${data.message}' />
+`)
