bce8ee41c2b49d4603f339b5dd52c17d76b31316
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-06-24 15:40:37 +0100

    linked the fetch request to a button that generates a new activity whenever pressed

--- API-BoredBot/index.js
+++ API-BoredBot/index.js
@@ -1,5 +1,11 @@
 const urlData = 'https://www.boredapi.com/api/activity'
+const fetchActivityButton = document.getElementById('fetchActivity')
 
+
+fetchActivityButton.innerText = 'New Activity'
+fetchActivityButton.addEventListener('click', () => {
+    
 fetch(urlData)
 .then(response => response.json())
-.then(data => document.getElementById('idea').innerHTML = `${data.activity}`)
+.then(data => {document.getElementById('activity').textContent = `${data.activity}`})
+})
