3fe30a1f22c5675ede0681ccbc980bf2d53c2257
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-06-24 14:05:50 +0100

    used the random dog api to genrate a random dog photo every page refresh

--- API-BoredBot/index.js
+++ API-BoredBot/index.js
@@ -1,3 +1,11 @@
+
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        document.querySelector('.image-div').innerHTML = `
+        <img src="${data.message}"/>
+        `
+    })
+
+
