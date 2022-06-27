db21661e2506f7853a9be8b2cd1fd7b44894c0b4
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-06-27 13:25:26 +0100

    made some small temporaty changes to the app, reversable and comented out

--- API-BoredBot/index.js
+++ API-BoredBot/index.js
@@ -4,8 +4,15 @@ const fetchActivityButton = document.getElementById('fetchActivity')
 
 fetchActivityButton.innerText = 'New Activity'
 fetchActivityButton.addEventListener('click', () => {
-    
+    //only makes the button visible when the function is invoked
+// document.querySelector('.whereActivity').style.display = 'block'
 fetch(urlData)
 .then(response => response.json())
-.then(data => {document.getElementById('activity').textContent = `${data.activity}`})
+.then(data => {
+    console.log(data)
+    document.getElementById('activity').textContent = `${data.activity}`
+    document.querySelector('.whereActivity').innerHTML = `
+                                            <a id="where-activity" href="${data.link === '' ? '#' : data.link}" target="_blank">Explain</a>
+                                            `
+})
 })
