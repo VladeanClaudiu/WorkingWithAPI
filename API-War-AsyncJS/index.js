0e73be8b2214cb27372adcd79e68f29f2895b893
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-07-01 14:48:49 +0100

    working with callbacks

--- API-War-AsyncJS/index.js
+++ API-War-AsyncJS/index.js
@@ -1,7 +1,37 @@
 const generateDeckBtn = document.getElementById('generate-deck');
 
-generateDeckBtn.addEventListener('click', () => {
+const fetchCards = () => {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
     .then(res => res.json())
-    .then(data => console.log(data));
-})
+    .then(data => console.log(data)); 
+}
+
+generateDeckBtn.addEventListener('click', fetchCards)
+
+const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+]
+
+function filterPet(value){
+    return value.hasPet;
+}
+
+
+function filterArray(array, callback){
+    const resultArray = [];
+    for(let item of array){
+        const callbackIncluded = callback(item);
+        if(callbackIncluded){
+            resultArray.push(item)
+        }
+    }
+    return resultArray;
+}
+
+const filterArrayVaiableTest = filterArray(people, (person) => person.hasPet);
+
+console.log(filterArrayVaiableTest)
+
