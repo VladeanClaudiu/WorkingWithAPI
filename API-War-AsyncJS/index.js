28cb564bc49de29771d8164b0917ebad0c3fd0da
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-07-04 17:15:49 +0100

    test changes

--- API-War-AsyncJS/index.js
+++ API-War-AsyncJS/index.js
@@ -1,37 +1,37 @@
-const generateDeckBtn = document.getElementById('generate-deck');
+const generateDeckBtn = document.getElementById("generate-deck");
 
 const fetchCards = () => {
-    fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
-    .then(res => res.json())
-    .then(data => console.log(data)); 
-}
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
 
-generateDeckBtn.addEventListener('click', fetchCards)
+generateDeckBtn.addEventListener("click", fetchCards);
 
 const people = [
-    { name: "Jack", hasPet: true },
-    { name: "Jill", hasPet: false },
-    { name: "Alice", hasPet: true },
-    { name: "Bob", hasPet: false },
-]
+  { name: "Jack", hasPet: true },
+  { name: "Jill", hasPet: false },
+  { name: "Alice", hasPet: true },
+  { name: "Bob", hasPet: false },
+];
 
-function filterPet(value){
-    return value.hasPet;
+function filterPet(value) {
+  return value.hasPet;
 }
 
-
-function filterArray(array, callback){
-    const resultArray = [];
-    for(let item of array){
-        const callbackIncluded = callback(item);
-        if(callbackIncluded){
-            resultArray.push(item)
-        }
+function filterArray(array, callback) {
+  const resultArray = [];
+  for (let item of array) {
+    const callbackIncluded = callback(item);
+    if (callbackIncluded) {
+      resultArray.push(item);
     }
-    return resultArray;
+  }
+  return resultArray;
 }
 
 const filterArrayVaiableTest = filterArray(people, (person) => person.hasPet);
 
-console.log(filterArrayVaiableTest)
+console.log(filterArrayVaiableTest);
 
+//repo test
