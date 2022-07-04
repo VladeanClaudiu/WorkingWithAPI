b1620d6048cb6492100d0aff39fd9afa4df686da
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-07-04 17:42:47 +0100

    assign deck id from the api, to a variable

--- API-War-AsyncJS/index.js
+++ API-War-AsyncJS/index.js
@@ -1,37 +1,13 @@
 const generateDeckBtn = document.getElementById("generate-deck");
+let deckId;
 
 const fetchCards = () => {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    });
 };
 
 generateDeckBtn.addEventListener("click", fetchCards);
-
-const people = [
-  { name: "Jack", hasPet: true },
-  { name: "Jill", hasPet: false },
-  { name: "Alice", hasPet: true },
-  { name: "Bob", hasPet: false },
-];
-
-function filterPet(value) {
-  return value.hasPet;
-}
-
-function filterArray(array, callback) {
-  const resultArray = [];
-  for (let item of array) {
-    const callbackIncluded = callback(item);
-    if (callbackIncluded) {
-      resultArray.push(item);
-    }
-  }
-  return resultArray;
-}
-
-const filterArrayVaiableTest = filterArray(people, (person) => person.hasPet);
-
-console.log(filterArrayVaiableTest);
-
-//repo test
