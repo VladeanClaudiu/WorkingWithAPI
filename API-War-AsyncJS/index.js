3a9e2a71a3ac026bfa51af257c2d73a949f28e29
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-07-04 18:13:41 +0100

    draw card function adds html, displaying the two images of the cards from the api

--- API-War-AsyncJS/index.js
+++ API-War-AsyncJS/index.js
@@ -1,5 +1,7 @@
 const generateDeckBtn = document.getElementById("generate-deck");
-let deckId;
+const drawCardsBtn = document.getElementById("draw-cards");
+const cardImageDiv = document.getElementById("card-img");
+let deckId = null;
 
 const fetchCards = () => {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -10,4 +12,26 @@ const fetchCards = () => {
     });
 };
 
+const drawCards = () => {
+  cardImageDiv.innerHTML = "";
+  fetch(
+    `https://apis.scrimba.com/deckofcards/api/deck//${deckId}/draw/?count=2`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      data.cards.map((card) => {
+        cardImageDiv.innerHTML += `<img src="${
+          card.image
+        }" alt="a image of a playing card the ${card.value + card.suit}">`;
+      });
+    });
+};
+
+//generates new deck
 generateDeckBtn.addEventListener("click", fetchCards);
+
+//draws cards from generated deck > does not allow draw is the deck is not generated yet
+drawCardsBtn.addEventListener("click", () =>
+  deckId === null ? alert("drawCards") : drawCards()
+);
