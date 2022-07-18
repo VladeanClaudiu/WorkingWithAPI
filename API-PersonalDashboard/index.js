72d0ba8559dcf1eb71ab6727ddbf4e4e5e4504af
Claudiu Vladean <claudiuver3.2@gmail.com>
2022-07-18 17:29:20 +0100

    used the unsplash api to acces images for background and background author

--- API-PersonalDashboard/index.js
+++ API-PersonalDashboard/index.js
@@ -1,3 +1,17 @@
-const bodySelect = document.querySelector("body");
+const apiLink =
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature";
+const photoAuthor = document.getElementById("author-info");
 
-bodySelect.innerHTML = "Hello Worlds";
+const setBackgroundImage = () => {
+  fetch(apiLink)
+    .then((res) => res.json())
+    .then((data) => {
+      let link = data.urls.regular;
+      let authorName = data.user.name;
+      console.log(data);
+      document.body.style.backgroundImage = `url(${link})`;
+      photoAuthor.innerHTML = `<p class="authorName">By: ${authorName}</p>`;
+    });
+};
+
+setBackgroundImage();
